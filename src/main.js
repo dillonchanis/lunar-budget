abf83c8334880d9a31e47516e3fb33ec0c8a232a
Dillon Chanis <dillonchanis92@gmail.com>
2017-12-30 19:15:10 -0500

    Getting rid of unused filter

--- src/main.js
+++ src/main.js
@@ -8,21 +8,6 @@ Chart.defaults.global.defaultFontColor = '#e6e8ff'
 
 Vue.config.productionTip = false
 
-Vue.filter('truncate', function (text, length = 30, clamp = '...') {
-  if (text.length <= length) return text
-
-  let tcText = text.slice(0, length - clamp.length)
-  let last = tcText.length - 1
-
-  while (last > 0 && tcText[last] !== ' ' && tcText[last] !== clamp[0]) last -= 1
-
-  last = last || length - clamp.length
-
-  tcText = tcText.slice(0, last)
-
-  return tcText + clamp
-})
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
