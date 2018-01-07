9d7634801af7c6ff8856c2d6afa56ee56f297efc
Dillon Chanis <dillonchanis92@gmail.com>
2018-01-06 20:08:24 -0500

    Allowing card to now accept classes from parent.

--- src/app/components/Presentational/Card.vue
+++ src/app/components/Presentational/Card.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <div class="bg-indigo-darkest border-indigo-darkest shadow-md rounded">
+  <div :class="props.className" class="bg-indigo-darkest border-indigo-darkest shadow-md rounded">
     <header class="bg-indigo-darker border-b border-indigo-darkest shadow flex justify-between rounded-tl rounded-tr px-4">
       <slot name="header"></slot>
     </header>
@@ -13,6 +13,12 @@
   export default {
     name: 'lunar-card',
 
-    functional: true
+    functional: true,
+
+    props: {
+      className: {
+        type: [String, Array],
+      }
+    }
   }
 </script>
